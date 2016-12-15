0b079883bcc2f2f2e26f4e7bbd3ab5bf216c70b1
CorentinA1 <corentin.barataud@viacesi.fr>
2016-12-15 13:34:07 +0100

    Add files via upload

---
+++ Lanceur/lanceur.h
@@ -0,0 +1,22 @@
+#ifndef LANCEUR_H
+#define LANCEUR_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+
+
+void afficherfinInfo();
+int save (int mode);
+int statique ();
+void menustat();
+void afficherInfo();
+//void afficherInfo();
+
+
+
+
+
+
+#endif
---
+++ Statique/statique.h
@@ -0,0 +1,16 @@
+#ifndef STATIQUE_H_INCLUDED
+#define STATIQUE_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+#endif
